42eefbb9c6ba908fe13f29ff50a0654ea1e13367
ethangreeneyy <ethan@greene.nz>
2025-03-19 22:46:24 +1300

    valid parentheses

---
+++ Stacks/20-ValidParentheses.java
@@ -0,0 +1,26 @@
+package Stacks;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        char[] stack = new char[s.length() + 1];
+        int index = -1;
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(') {
+                stack[++index] = ')';
+            } else if (c == '{') {
+                stack[++index] = '}';
+            } else if (c == '[') {
+                stack[++index] = ']';
+            } else if (index == -1 || stack[index--] != c) {
+                return false;
+            }
+
+        }
+
+        return index == -1;
+
+    }
+}
