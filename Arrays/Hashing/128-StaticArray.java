61eff6fd55475c03eb840076edb5483cc1afb5d5
ethangreeneyy <ethan@greene.nz>
2025-03-21 12:31:12 +1300

    128 working for non negative values

---
+++ Arrays/Hashing/128-StaticArray.java
@@ -0,0 +1,37 @@
+class Solution {
+    public static int longestConsecutive(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int max = nums[0];
+        int min = nums[0];
+
+        for (int i : nums) {
+            max = Math.max(max, i);
+            min = Math.min(min, i);
+        }
+
+        var res = new boolean[max - min + 1];
+
+        for (int i : nums) {
+            res[i - min] = true;
+        }
+
+        int leftPointer = 0;
+        int rightPointer = 0;
+        int maxLength = 0;
+
+        for (; rightPointer < res.length; rightPointer++) {
+            if (res[rightPointer] == true) {
+                maxLength = Math.max(rightPointer - leftPointer + 1, maxLength);
+            } else {
+                leftPointer = rightPointer;
+            }
+        }
+
+        return maxLength;
+
+    }
+}
