165924e8927a9edb61b9bf3720f82da49f62ccd8
ethangreeneyy <ethan@greene.nz>
2025-03-21 15:46:05 +1300

    works for most test cases, but time lmit exceeded on others

---
+++ Arrays/Hashing/128-HashMap.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        var numsSet = new HashSet<Integer>();
+
+        for (int i : nums) {
+            numsSet.add(i);
+        }
+
+        int maxLen = 1;
+
+        for (int i : numsSet) {
+            int currentLen = 1;
+            while (numsSet.contains(--i)) {
+                currentLen++;
+            }
+            maxLen = Math.max(maxLen, currentLen);
+        }
+
+        return maxLen;
+
+    }
+}
