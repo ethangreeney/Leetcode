cb8a20fb9e90178e5a31939415d93a555802eaeb
ethangreeneyy <ethan@greene.nz>
2025-05-19 09:18:11 +1200

    more idiomatic recursive reverse linked list solution

--- LinkedList/206-ReverseLinkedList.java
+++ LinkedList/206-ReverseLinkedList.java
@@ -19,7 +19,25 @@ class ListNode {
     }
 }
 
-class Solution {
+class RecursiveMoreIdiomatic {
+    public ListNode reverseList(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next);
+
+        head.next.next = head;
+
+        head.next = null;
+
+        return newHead;
+
+    }
+}
+
+class Recursive {
     public ListNode reverseList(ListNode head) {
 
         if (head == null || head.next == null) {
