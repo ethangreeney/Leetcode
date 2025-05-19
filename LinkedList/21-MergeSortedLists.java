a67aeba2faaddf992f75e4c82dec24fa83f136fb
ethangreeneyy <ethan@greene.nz>
2025-05-19 19:06:54 +1200

    more concise merge sorted lists solution

--- LinkedList/21-MergeSortedLists.java
+++ LinkedList/21-MergeSortedLists.java
@@ -19,7 +19,38 @@ class ListNode {
     }
 }
 
-class Solution {
+class ConsiseIterativeSolution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode newHead = new ListNode();
+        ListNode current = newHead;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return newHead.next;
+
+    }
+}
+
+class InitialSolution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
         if (list1 == null && list2 == null) {
