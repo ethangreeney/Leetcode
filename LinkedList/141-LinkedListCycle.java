39857398abff76a0cbc912e4a53e8ef706c1e6cb
ethangreeneyy <ethan@greene.nz>
2025-05-20 16:40:40 +1200

    optimised two pointers linked list cycle

--- LinkedList/141-LinkedListCycle.java
+++ LinkedList/141-LinkedListCycle.java
@@ -20,6 +20,32 @@ class ListNode {
     }
 }
 
+class TwoPointers {
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+
+        }
+
+        return false;
+
+    }
+}
+
 class IterativeHashSet {
     public boolean hasCycle(ListNode head) {
 
