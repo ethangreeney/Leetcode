4e47de7550c6a8eb7384556b22295bf334ae2dad
ethangreeneyy <ethan@greene.nz>
2025-05-21 17:00:32 +1200

    concise DFS max tree depth

--- Trees/104-MaxDepthBinTree.java
+++ Trees/104-MaxDepthBinTree.java
@@ -1,5 +1,37 @@
+//problem link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
 package Trees;
 
-public class 104-MaxDepthBinTree {
-    
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class DFS {
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int leftSubtree = maxDepth(root.left);
+        int rightSubtree = maxDepth(root.right);
+
+        return leftSubtree > rightSubtree ? ++leftSubtree : ++rightSubtree;
+
+    }
 }
