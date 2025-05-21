75bd67633809d7aad7ba29eec0fe95be16ebc51a
ethangreeneyy <ethan@greene.nz>
2025-05-21 17:07:37 +1200

    max tree depth one liner

--- Trees/104-MaxDepthBinTree.java
+++ Trees/104-MaxDepthBinTree.java
@@ -35,3 +35,9 @@ class DFS {
 
     }
 }
+
+class OneLiner {
+    public int maxDepth(TreeNode root) {
+        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
