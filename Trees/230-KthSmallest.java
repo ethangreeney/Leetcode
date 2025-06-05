bc16c7dc0b46c9c4ded24ab6c25ee2aa7f34b922
ethangreeneyy <ethan@greene.nz>
2025-06-06 11:06:29 +1200

    kth smallest in BST beats 100%

---
+++ Trees/230-KthSmallest.java
@@ -0,0 +1,33 @@
+// problem link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+package Trees;
+
+class Solution {
+
+    private TreeNode res;
+    private int k;
+
+    public int kthSmallest(TreeNode root, int k) {
+
+        this.k = k;
+        inOrderHelper(root);
+        return res.val;
+
+    }
+
+    private void inOrderHelper(TreeNode root) {
+
+        if (root == null) {
+            return;
+        }
+
+        inOrderHelper(root.left);
+
+        if (k-- == 1) {
+            res = root;
+        }
+
+        inOrderHelper(root.right);
+
+    }
+}
