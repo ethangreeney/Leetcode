7fba54d649492e985e974c2dd827c58a5a715fbb
ethangreeneyy <ethan@greene.nz>
2025-06-09 09:32:09 +1200

    3 sum cpp wip

---
+++ RandomStuff/randomCpp.cpp
@@ -0,0 +1,11 @@
+#include <string>
+class test {
+ public:
+  void main() {
+    int x = 10, y = 20;
+
+    std::string = "Ethan";
+
+    std::cout
+  }
+};
---
+++ TwoPointers/13-3Sum.cpp
@@ -0,0 +1,5 @@
+#include <vector>
+class Solution {
+ public:
+  vector<vector<int>> threeSum(vector<int>& nums) { nums. }
+};
