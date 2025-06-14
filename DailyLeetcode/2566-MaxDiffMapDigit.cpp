b35d54aa6360e8d6bdac390c27e3ea61260c3b8c
ethangreeneyy <ethan@greene.nz>
2025-06-15 11:40:43 +1200

    max difference by remapping beats 100%

--- DailyLeetcode/2566-MaxDiffMapDigit.cpp
+++ DailyLeetcode/2566-MaxDiffMapDigit.cpp
@@ -1,7 +1,52 @@
-class Solution
-{
-public:
-    int minMaxDifference(int num)
-    {
+// problem link:
+// https://leetcode.com/problems/maximum-difference-by-remapping-a-digit/
+
+#include <vector>
+
+class Solution {  // 0ms beats 100%
+ public:
+  int minMaxDifference(int num) {
+    std::string maxNum = std::to_string(num);
+
+    char firstBelowNine;
+    bool found = false;
+
+    for (int i = 0; i < maxNum.length(); i++) {
+      if (!found) {
+        if (maxNum[i] != '9') {
+          firstBelowNine = maxNum[i];
+          maxNum[i] = '9';
+          found = true;
+        }
+      } else {
+        if (maxNum[i] == firstBelowNine) {
+          maxNum[i] = '9';
+        }
+      }
     }
+
+    int max = std::stoi(maxNum);
+
+    std::string minNum = std::to_string(num);
+    char firstAboveZero;
+    found = false;
+
+    for (int i = 0; i < minNum.length(); i++) {
+      if (!found) {
+        if (minNum[i] != '0') {
+          firstAboveZero = minNum[i];
+          minNum[i] = '0';
+          found = true;
+        }
+      } else {
+        if (minNum[i] == firstAboveZero) {
+          minNum[i] = '0';
+        }
+      }
+    }
+
+    int min = std::stoi(minNum);
+
+    return max - min;
+  }
 };
