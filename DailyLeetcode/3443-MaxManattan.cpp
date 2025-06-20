ad68a9a620e8f4db3b0e3f87a128ad0282e0d655
ethangreeneyy <ethan@greene.nz>
2025-06-20 22:09:11 +1200

    improved variable name, added link

--- DailyLeetcode/3443-MaxManattan.cpp
+++ DailyLeetcode/3443-MaxManattan.cpp
@@ -1,3 +1,6 @@
+// problem link:
+// https://leetcode.com/problems/maximum-manhattan-distance-after-k-changes
+
 #include <array>
 #include <string>
 
@@ -6,22 +9,17 @@ class Solution {  // 54ms beats 88%
   int maxDistance(std::string s, int k) {
     int north = 0, east = 0, south = 0, west = 0;
 
-    int maxDistance = 0, currentDistance = 0;
+    int maxDistance = 0, currentDistance = 0, wastefulMoves = 0;
 
     for (char c : s) {
       if (c == 'N') north++;
       if (c == 'E') east++;
       if (c == 'S') south++;
       if (c == 'W') west++;
-
       currentDistance = std::abs(north - south) + std::abs(east - west);
-
-      int possibleImprovments = std::min(north, south) + std::min(east, west);
-
-      possibleImprovments = std::min(2 * possibleImprovments, 2 * k);
-
-      maxDistance =
-          std::max(maxDistance, currentDistance + possibleImprovments);
+      wastefulMoves = std::min(north, south) + std::min(east, west);
+      wastefulMoves = std::min(2 * wastefulMoves, 2 * k);
+      maxDistance = std::max(maxDistance, currentDistance + wastefulMoves);
     }
 
     return maxDistance;
