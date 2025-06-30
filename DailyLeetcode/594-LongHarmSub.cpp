46bad35e6033f9744fcb7c7e1bf79b4f147433f7
ethangreeneyy <ethan@greene.nz>
2025-06-30 17:08:16 +1200

    longest harmonious sub beats 34%

---
+++ DailyLeetcode/594-LongHarmSub.cpp
@@ -0,0 +1,37 @@
+// problem link:
+// https://leetcode.com/problems/longest-harmonious-subsequence/description/
+
+#include <unordered_map>
+#include <vector>
+
+class Solution {  // 35ms beats 34%
+ public:
+  int findLHS(std::vector<int>& nums) {
+    if (nums.size() == 1) {
+      return 0;
+    }
+
+    std::unordered_map<int, int> freq;
+
+    for (int i : nums) {
+      freq[i]++;
+    }
+
+    std::vector<std::pair<int, int>> entrySet;
+
+    for (const std::pair<int, int> pair : freq) {
+      entrySet.push_back(pair);
+    }
+
+    std::sort(entrySet.begin(), entrySet.end());
+
+    int maxSub = 0;
+
+    for (int i = 0; i < entrySet.size() - 1; i++) {
+      if (entrySet[i + 1].first - entrySet[i].first == 1) {
+        maxSub = std::max(maxSub, entrySet[i + 1].second + entrySet[i].second);
+      }
+    }
+    return maxSub;
+  }
+};
