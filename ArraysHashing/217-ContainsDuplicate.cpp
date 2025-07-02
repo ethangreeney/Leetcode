c54223ca9a6fe4b43e3a26334c1eeb1be8167cde
ethangreeneyy <ethan@greene.nz>
2025-07-03 10:19:20 +1200

    contains duplicate

---
+++ ArraysHashing/217-ContainsDuplicate.cpp
@@ -0,0 +1,19 @@
+// problem link: https://leetcode.com/problems/contains-duplicate/
+
+#include <unordered_set>
+
+class Solution {  // 43ms beats 74% (best O(n) time possible)
+ public:
+  bool containsDuplicate(std::vector<int>& nums) {
+    std::unordered_set<int> hashSet;
+    hashSet.reserve(nums.size());
+
+    for (int i : nums) {
+      if (hashSet.contains(i)) {
+        return true;
+      }
+      hashSet.insert(i);
+    }
+    return false;
+  }
+};
