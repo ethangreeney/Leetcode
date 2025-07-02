0dd6f48a5bc570f8b5defee7efb845064ce2d260
ethangreeneyy <ethan@greene.nz>
2025-07-02 13:28:08 +1200

    coin change more concise, better cache locality

--- 1D-DP/322-CoinChange.cpp
+++ 1D-DP/322-CoinChange.cpp
@@ -2,6 +2,22 @@
 
 #include <vector>
 
+class SolutionBetterCacheLocality {  // 11ms beats 98%
+ public:
+  int coinChange(std::vector<int>& coins, int amount) {
+    std::vector<int> DP(amount + 1, amount + 1);
+
+    DP[0] = 0;
+
+    for (const auto& coin : coins) {
+      for (int i = coin; i <= amount; i++) {
+        DP[i] = std::min(DP[i], DP[i - coin] + 1);
+      }
+    }
+    return DP[amount] == amount + 1 ? -1 : DP[amount];
+  }
+};
+
 class Solution {  // 11ms beats 98%
  public:
   int coinChange(std::vector<int>& coins, int amount) {
