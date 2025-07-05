b7df825b25de6d59adf12cb1181048296839ec6c
ethangreeneyy <ethan@greene.nz>
2025-07-06 10:56:34 +1200

    trapping rain water 0ms beats 100%

--- TwoPointers/42-TrappingRainWater.cpp
+++ TwoPointers/42-TrappingRainWater.cpp
@@ -2,6 +2,37 @@
 
 #include <vector>
 
+class Solution {  // 0ms beats 100%
+ public:
+  int trap(std::vector<int>& height) {
+    int l = 0;
+    int r = height.size() - 1;
+
+    int maxL = 0;
+    int maxR = 0;
+
+    int totalWater = 0;
+
+    while (l <= r) {
+      if (maxL < maxR) {
+        if (height[l] < maxL) {
+          totalWater += maxL - height[l];
+        }
+        maxL = std::max(maxL, height[l]);
+        ++l;
+      } else {
+        if (height[r] < maxR) {
+          totalWater += maxR - height[r];
+        }
+        maxR = std::max(maxR, height[r]);
+        --r;
+      }
+    }
+
+    return totalWater;
+  }
+};
+
 class Solution {  // only works for one test case
  public:
   int trap(std::vector<int>& height) {
